955ffa9574cf11ffa122e07898e9808c2094c985
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-06-15 14:57:36 +0300

    edit compose and index

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Hannu!</h1>')
+  res.send('<h1>Moi Hannu!</h1>')
 })
 
 const PORT = 8080
